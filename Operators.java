a21f34a1d9faf15af6f6d9c7051a92d6e4cb5374
Naveen kumar <237y1a05j0@mlritm.ac.in>
2025-08-01 12:06:00 +0530

    Operators.java

---
+++ Operators.java
@@ -0,0 +1,12 @@
+public class ArithmeticExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("a + b = " + (a + b));  // 13
+        System.out.println("a - b = " + (a - b));  // 7
+        System.out.println("a * b = " + (a * b));  // 30
+        System.out.println("a / b = " + (a / b));  // 3 (integer division)
+        System.out.println("a % b = " + (a % b));  // 1
+    }
+}
